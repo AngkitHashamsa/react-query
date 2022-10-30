94d77bb8e8a78b55015abc68f36367e63b000424
Angkit Hashamsa <Angkithashamsa21@gmail.com>
2022-10-30 23:08:46 +0530

    data transformation

--- src/pages/ReactQuery.js
+++ src/pages/ReactQuery.js
@@ -8,31 +8,28 @@ const url = "http://localhost:4000/details";
 const getDetails = () => axios.get(url);
 
 const ReactQuery = () => {
-  // cache time default is 5 min
+  // select help us to target a specific data from the parent data
   const { data, isLoading, error, isError, isFetching } = useQuery(
     "details",
-    getDetails
-    // {
-    //   cacheTime: 5000,
-    // }
+    getDetails,
+    {
+      select: (data) => {
+        const title = data.data.map((details) => details.title);
+        return title;
+      },
+    }
   );
 
   console.log({ isFetching, isLoading }, "isFetching");
 
   if (isLoading) return <h2>Loading......</h2>;
   if (isError) return <h2>{error?.message}</h2>;
-
+  console.log(data);
   return (
     <div>
       <NavBar />
       ReactQuery
-      {data &&
-        data?.data.slice(0, 5).map((item) => (
-          <div key={item?.id}>
-            {item?.id}
-            <p>{item?.title}</p>
-          </div>
-        ))}
+      {data && data.slice(0, 5).map((title) => <h2 key={title}>{title}</h2>)}
     </div>
   );
 };
