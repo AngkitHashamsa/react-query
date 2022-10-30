ba8d3b96e876d6413323d2509cdb567e1b3d0676
Angkit Hashamsa <Angkithashamsa21@gmail.com>
2022-10-30 21:21:06 +0530

    intial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Home = () => {
+  return (
+    <div>
+      Home
+      <Link to="/query">React-Query</Link>
+    </div>
+  );
+};
+
+export default Home;
